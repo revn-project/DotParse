4c3cb7c51bfb6abe9d2633676680aecabb78c4a7
Kohei Hakoishi <wipiano@gmail.com>
2017-08-02 19:30:04 +0900

    Position.NoPosition

--- DotParse2/Input/Position.cs
+++ DotParse2/Input/Position.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using DotParse.Utils;
 
 namespace DotParse.Input
 {
     public abstract class Position : IPosition
     {
+        public static readonly Position NoPosition = new NoPositionImpl();
+
         public abstract int Line { get; }
 
         public abstract int Column { get; }
 
-        public string LongString
+        public virtual string LongString
             => $"{this.LineContents}\n{this.LineContents.Take(this.Column - 1).Select(x => x == '\t' ? x : ' ').Join()}^";
 
         protected abstract string LineContents { get; }
@@ -27,5 +28,18 @@ namespace DotParse.Input
 
         public override string ToString()
             => $"{this.Line}.{this.Column}";
+
+        private class NoPositionImpl : Position
+        {
+            public override int Line => 0;
+
+            public override int Column => 0;
+
+            public override string ToString() => "<undefined position>";
+
+            public override string LongString => this.ToString();
+
+            protected override string LineContents => string.Empty;
+        }
     }
 }
