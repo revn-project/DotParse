09f40708e16ac5bcf3f73cb0e87b99f389382241
Kohei Hakoishi <wipiano@gmail.com>
2017-08-02 19:43:20 +0900

    IPositional

---
+++ DotParse2/Input/IPositional.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace DotParse.Input
+{
+    public interface IPositional
+    {
+        IPosition Pos { get; }
+
+        IPositional SetPos(IPosition newpos);
+    }
+
+    public abstract class IPositionalDefaultImpl : IPositional
+    {
+        public IPosition Pos { get; protected set; } = Position.NoPosition;
+
+        public IPositional SetPos(IPosition newPos)
+        {
+            if (this.Pos == Position.NoPosition)
+            {
+                this.Pos = Position.NoPosition;
+            }
+
+            return this;
+        }
+    }
+}
