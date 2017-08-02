8f5c7b72e88833d6020fef59e3873b9fd71f281b
Kohei Hakoishi <wipiano@gmail.com>
2017-08-02 19:19:33 +0900

    add IEnumerable<char>.Join()

---
+++ DotParse2/Utils/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DotParse.Utils
+{
+    internal static class StringExtensions
+    {
+        public static string Join(this IEnumerable<char> that)
+            => string.Join(string.Empty, that);
+    }
+}
