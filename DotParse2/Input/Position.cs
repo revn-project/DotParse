b014f213ce1a43bbfcf8ff4e15c66d6f4d0c3c21
Kohei Hakoishi <wipiano@gmail.com>
2017-08-02 19:23:53 +0900

    add abstract Position

---
+++ DotParse2/Input/Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using DotParse.Utils;
+
+namespace DotParse.Input
+{
+    public abstract class Position : IPosition
+    {
+        public abstract int Line { get; }
+
+        public abstract int Column { get; }
+
+        public string LongString
+            => $"{this.LineContents}\n{this.LineContents.Take(this.Column - 1).Select(x => x == '\t' ? x : ' ').Join()}^";
+
+        protected abstract string LineContents { get; }
+
+
+        public int CompareTo(IPosition that)
+        {
+            var lineResult = this.Line - that.Line;
+
+            return lineResult == 0
+                ? this.Column - that.Column
+                : lineResult;
+        }
+
+        public override string ToString()
+            => $"{this.Line}.{this.Column}";
+    }
+}
