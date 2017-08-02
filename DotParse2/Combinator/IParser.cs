a5b2029a24675dfeb184e0a0b00ce46861556e12
Akira Kido <achts.y@gmail.com>
2017-08-02 19:57:00 +0900

    パーサに手を付けだした

---
+++ DotParse2/Combinator/IParser.cs
@@ -0,0 +1,9 @@
+﻿namespace DotParse.Combinator
+{
+
+    public interface IParser
+    {
+        
+    }
+
+}
---
+++ DotParse2/Combinator/Parser.cs
@@ -0,0 +1,9 @@
+﻿namespace DotParse.Combinator
+{
+
+    public class Parser
+    {
+        
+    }
+
+}
