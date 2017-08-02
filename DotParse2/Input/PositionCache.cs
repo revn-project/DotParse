5a519f73c91ddd96bc80891b7519b0e4a567eb5d
Kohei Hakoishi <wipiano@gmail.com>
2017-08-02 20:15:36 +0900

    add a weak position cache

---
+++ DotParse2/Input/PositionCache.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace DotParse.Input
+{
+    internal sealed class PositionCache
+    {
+        private readonly ConditionalWeakTable<IEnumerable<char>, int[]> _cache;
+
+        public PositionCache()
+        {
+            _cache = new ConditionalWeakTable<IEnumerable<char>, int[]>();
+        }
+
+        public int[] Get(IEnumerable<char> key) => _cache.TryGetValue(key, out int[] result) ? result : null;
+
+        public void Add(IEnumerable<char> key, int[] value) => _cache.Add(key, value);
+    }
+}
